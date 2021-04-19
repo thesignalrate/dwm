85eb64dda42a9eea77d68a4dd799da4517dfc242
ahmet <thesignalrate@gmail.com>
2021-04-19 20:47:28 +0300

    fix indent

--- config.def.h
+++ config.def.h
@@ -97,86 +97,86 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[]  = { "/bin/sh", "-c", "~/.dwm/bin/terminal", NULL };
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_f,      zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_Escape,     killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-//	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-//	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-/// centeredmaster_begin
-//	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
-//	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
-/// centeredmaster_end
-/// cyclelayouts_begin
-	{ MODKEY,		    XK_Down,  cyclelayout,    {.i = -1 } },
-	{ MODKEY,           XK_Up,    cyclelayout,    {.i = +1 } },
-/// cyclelayouts_end
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-/// actualfullscreen_begin
-	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
-/// actualfullscreen_end
-/// alwaysontop_begin
-	{ ALTKEY|ShiftMask,             XK_t,  togglealwaysontop, {0} },
-    { ALTKEY|ShiftMask|ControlMask,             XK_t, unalwaysontop_all, {0} },
-/// alwaysontop_end
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-/// clientopacity_begin
-//    { MODKEY|ShiftMask,             XK_KP_Add, changeopacity,  {.f = +0.1}},
-//    { MODKEY|ShiftMask,             XK_KP_Subtract, changeopacity,  {.f = -0.1}},
-/// clientopacity_end
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-/// functionalgap_begin
-	{ MODKEY|ShiftMask,             XK_KP_Add,  setgaps,        {.i = -5 } },
-	{ MODKEY|ShiftMask,             XK_KP_Subtract,  setgaps,         {.i = +5 } },
-	{ MODKEY|ShiftMask,                       XK_KP_Equal,  setgaps,        {.i = GAP_RESET } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = GAP_TOGGLE} },
-/// functionalgap_end
-/// shiftview_begin
-	{ MODKEY,              XK_Right,           shiftview,  { .i = +1 } },
-	{ MODKEY,              XK_Left,           shiftview,  { .i = -1 } },
-	{ MODKEY|ShiftMask, XK_Left, rotatestack, {.i = -1} },
-	{ MODKEY|ShiftMask, XK_Right, rotatestack, {.i = +1} },
-/// shiftview_end
-/// maximize_begin
-	{ MODKEY|ControlMask|ShiftMask, XK_h,           togglehorizontalmax, {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_l,           togglehorizontalmax, {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_j,           toggleverticalmax,   {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_k,           toggleverticalmax,   {0} },
-	{ MODKEY|ControlMask,           XK_m,           togglemaximize,      {0} },
-/// maximize_end
-/// alternativetags_begin
-	{ MODKEY|ShiftMask,                       XK_n,      togglealttag,   {0} },
-/// alternativetags_end
-/// defaultopacity_begin
-//	{ MODKEY|ShiftMask,		XK_s,	   spawn,	   SHCMD("transset-df -a --dec .1") },
-//	{ MODKEY|ShiftMask,		XK_d,	   spawn,	   SHCMD("transset-df -a --inc .1") },
-//	{ MODKEY|ShiftMask,		XK_f,	   spawn,	   SHCMD("transset-df -a .75") },
-/// defaultopacity_end
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+  /* modifier                     key        function        argument */
+  { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+  { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+  { MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
+  { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+  { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+  { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+  { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+  { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+  { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+  { MODKEY,                       XK_f,      zoom,           {0} },
+  { MODKEY,                       XK_Tab,    view,           {0} },
+  { MODKEY,                       XK_Escape,     killclient,     {0} },
+  { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+  //	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+  //	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+  /// centeredmaster_begin
+  //	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
+  //	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
+  /// centeredmaster_end
+  /// cyclelayouts_begin
+  { MODKEY,		    XK_Down,  cyclelayout,    {.i = -1 } },
+  { MODKEY,           XK_Up,    cyclelayout,    {.i = +1 } },
+  /// cyclelayouts_end
+  { MODKEY,                       XK_space,  setlayout,      {0} },
+  { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+  /// actualfullscreen_begin
+  { MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
+  /// actualfullscreen_end
+  /// alwaysontop_begin
+  { MODKEY|ShiftMask,             XK_t,  togglealwaysontop, {0} },
+  { MODKEY|ShiftMask|ControlMask,             XK_t, unalwaysontop_all, {0} },
+  /// alwaysontop_end
+  { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+  { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+  { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+  { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+  /// clientopacity_begin
+  //    { MODKEY|ShiftMask,             XK_KP_Add, changeopacity,  {.f = +0.1}},
+  //    { MODKEY|ShiftMask,             XK_KP_Subtract, changeopacity,  {.f = -0.1}},
+  /// clientopacity_end
+  { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+  { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+  /// functionalgap_begin
+  { MODKEY|ShiftMask,             XK_KP_Add,  setgaps,        {.i = -5 } },
+  { MODKEY|ShiftMask,             XK_KP_Subtract,  setgaps,         {.i = +5 } },
+  { MODKEY|ShiftMask,                       XK_KP_Equal,  setgaps,        {.i = GAP_RESET } },
+  { MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = GAP_TOGGLE} },
+  /// functionalgap_end
+  /// shiftview_begin
+  { MODKEY,              XK_Right,           shiftview,  { .i = +1 } },
+  { MODKEY,              XK_Left,           shiftview,  { .i = -1 } },
+  { MODKEY|ShiftMask, XK_Left, rotatestack, {.i = -1} },
+  { MODKEY|ShiftMask, XK_Right, rotatestack, {.i = +1} },
+  /// shiftview_end
+  /// maximize_begin
+  { MODKEY|ControlMask|ShiftMask, XK_h,           togglehorizontalmax, {0} },
+  { MODKEY|ControlMask|ShiftMask, XK_l,           togglehorizontalmax, {0} },
+  { MODKEY|ControlMask|ShiftMask, XK_j,           toggleverticalmax,   {0} },
+  { MODKEY|ControlMask|ShiftMask, XK_k,           toggleverticalmax,   {0} },
+  { MODKEY|ControlMask,           XK_m,           togglemaximize,      {0} },
+  /// maximize_end
+  /// alternativetags_begin
+  { MODKEY|ShiftMask,                       XK_n,      togglealttag,   {0} },
+  /// alternativetags_end
+  /// defaultopacity_begin
+  //	{ MODKEY|ShiftMask,		XK_s,	   spawn,	   SHCMD("transset-df -a --dec .1") },
+  //	{ MODKEY|ShiftMask,		XK_d,	   spawn,	   SHCMD("transset-df -a --inc .1") },
+  //	{ MODKEY|ShiftMask,		XK_f,	   spawn,	   SHCMD("transset-df -a .75") },
+  /// defaultopacity_end
+  TAGKEYS(                        XK_1,                      0)
+  TAGKEYS(                        XK_2,                      1)
+  TAGKEYS(                        XK_3,                      2)
+  TAGKEYS(                        XK_4,                      3)
+  TAGKEYS(                        XK_5,                      4)
+  TAGKEYS(                        XK_6,                      5)
+  TAGKEYS(                        XK_7,                      6)
+  TAGKEYS(                        XK_8,                      7)
+  TAGKEYS(                        XK_9,                      8)
+  { MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
 /* button definitions */
