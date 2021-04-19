e3ecccddc161d60f37bf7d02efa3b8d2a3162779
ahmet <thesignalrate@gmail.com>
2021-04-19 20:08:32 +0300

    border

--- config.def.h
+++ config.def.h
@@ -1,9 +1,9 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 5;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const int startwithgaps	     = 1;	    /* 1 means gaps are used by default */
-static const unsigned int gappx     = 20;       /* default gap between windows in pixels */
+static const unsigned int gappx     = 5;       /* default gap between windows in pixels */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
