77874a0ad36e34a512aa2e9c8dd7aed76f45f6a0
ahmet <thesignalrate@gmail.com>
2021-04-19 20:12:02 +0300

    font fix

--- config.h
+++ config.h
@@ -8,8 +8,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const double defaultopacity  = 0.75;     
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Noto Sans:size=10" };
+static const char dmenufont[]       = "Noto Sans:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
